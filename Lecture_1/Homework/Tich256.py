8a3f874bd5eadf6fb32ae3051c7285e27f1a2a44
buianhduc <buianhduc.popodoo2@gmail.com>
2020-05-16 21:59:45 +0700

    Update Tich256.py

--- Lecture_1/Homework/Tich256.py
+++ Lecture_1/Homework/Tich256.py
@@ -3,4 +3,4 @@ for i in range(len(InitArr)):
     for j in range(i+1,len(InitArr)):
         
         if(InitArr[i]*InitArr[j]==256): #check if mutipling two elements equals 256
-            print("{0}x{1}".format(InitArr[i],InitArr[j])) #print
+            print("{0} x {1} at the positions {2} and {3}, respectively".format(InitArr[i],InitArr[j],i+1,j+1)) #print
