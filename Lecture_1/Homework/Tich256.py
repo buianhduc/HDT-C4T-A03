3369c187b58b4a3af430b769be1bf7c52c2a468e
buianhduc <buianhduc.popodoo2@gmail.com>
2020-05-17 09:28:18 +0700

    Update Tich256.py

--- Lecture_1/Homework/Tich256.py
+++ Lecture_1/Homework/Tich256.py
@@ -1,4 +1,10 @@
+def DeleteRepeatedElements(x):
+    return list(dict.fromkeys(x))
+
 InitArr =  [1,3,4,16,32,8,64,4,128,2,256,32] #initialize the list
+
+InitArr = DeleteRepeatedElements(InitArr)
+
 for i in range(len(InitArr)): 
     for j in range(i+1,len(InitArr)):
         
